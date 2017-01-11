4d4860979b402182531811d540d31c76a6b45e3b
Kevin Tran <ktran@tack.tech>
2017-01-10 17:51:45 -0800

    finish exercise 2

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -2,15 +2,34 @@
 
 # Define a function `CompareLength` that takes in 2 character strings, and returns
 # the sentence of the form
+
+CompareLength <- function(str1, str2) {
+  difference <- abs(nchar(str1) - nchar(str2));
+  print(paste("The difference in length is", difference));
+}
+
 #   "The difference in length is N"
 
 
 # Pass two strings of different lengths to your `CompareLength` function
+
+CompareLength("jawlkfas", "oikl")
 
 
 # Define a function `DescribeDifference` that will return one of the following statements:
 # "Your first string is longer by N characters"
 # "Your second string is longer by N characters"
 
+DescribeDifference <- function(str1, str2) {
+  difference <- abs(nchar(str1) - nchar(str2));
+  if(nchar(str1) > nchar(str2)) {
+    print(paste("Your first string is longer by", difference, "characters"));
+  } else {
+    print(paste("Your second string is longer by", difference, "characters"));
+  }
+}
+
 
 # Pass two strings of different lengths to your `DescribeDifference` function
+
+DescribeDifference("alksjdlkwaeafdsa3","jlksdfklasds")
